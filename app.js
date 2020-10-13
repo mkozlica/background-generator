4ed391c75de3e91d1215b30cf20aef352eaa837c
Marko Kozlica <markokozlica@Markos-MacBook.local>
2020-10-13 21:01:32 +0200

    jebem ti git

---
+++ app.js
@@ -0,0 +1,63 @@
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+//Event listeners
+todoButton.addEventListener("click", addTodo);
+
+todoList.addEventListener("click", deleteCheck);
+
+//Functions
+function addTodo(event) {
+  // Prevent form from submitting
+  event.preventDefault();
+  if (todoInput.value.length > 0) {
+    // Todo DIV
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    //create LI
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+
+    //check mark BUTTON
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    //trash BUTTON
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    //appen to LIST
+    todoList.appendChild(todoDiv);
+
+    //clear input value
+    todoInput.value = "";
+  }
+}
+function deleteCheck(e) {
+  const item = e.target;
+
+  //delete todo
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    //animation
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", function () {
+      todo.remove();
+    });
+  }
+
+  //check mark
+
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
+}
